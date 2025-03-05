b3ab68c9929cc0c1204be6e0290b72acfb771900
Abdullah <Abdullah@DESKTOP-51CVATC>
2025-03-05 07:24:08 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlishbaConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fact = 1;
+            Console.WriteLine("Enter Number");
+            int number = int.Parse(Console.ReadLine());
+            for(int i = number; i >= 1; i--) 
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("The Factorial Number is:(0)", fact);
+        }
+    }
+}
